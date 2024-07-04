378f87f8a7cea432a5e69a28deaf50c7b889d751
Paco <psalido@gmail.com>
2024-07-04 11:44:27 +0200

    i18n support and basic customizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,51 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "NOICETHEMES",
+      logo: {
+        src: "./src/assets/noice-logo.svg",
+      },
+      logo: {
+        light: "./src/assets/noice-logo.svg",
+        dark: "./src/assets/noice-logo-dark.svg",
+      },
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
+      // Set English as the default language for this site.
+      defaultLocale: "root",
+      locales: {
+        root: {
+          label: "English",
+          lang: "en", // lang is required for root locales
+        },
+        // Spanish docs in `src/content/docs/es/`
+        es: {
+          label: "Español",
+          lang: "es",
+        },
+      },
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
